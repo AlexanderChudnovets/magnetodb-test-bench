df81d61a6b60570752f351a2abbaafa24bfc8e50
Alexander Chudnovets <alexander_chudnovets@symantec.com>
2014-08-21 15:48:40 +0300

    Refactored runner

--- benchmark_runner.py
+++ benchmark_runner.py
@@ -1,159 +1,181 @@
-import datetime
 import ConfigParser
-import os
+import importlib
+import sys
+
 import re
 import subprocess
-import sys
-import uuid
+
+import datetime
+import os
 
 from fabric.operations import run, sudo, put
 from fabric.context_managers import settings, prefix, cd
+import uuid
+
+class BenchmarkRunner(object):
+    def __init__(self, results_dir, cfg, benchmark, monitor):
+        self.results_dir = results_dir
+        self.benchmark = benchmark
+        self.cfg = cfg
+        self.monitor = monitor
+
+    def setup(self):
+        self.benchmark.setup()
+
+    def cleanup(self):
+        self.benchmark.cleanup()
+
+    def save_results(self):
+        print "Store results"
+        os.rename(self.results_dir, '%s_%s' % (self.results_dir, get_timestamp()))
+
+    def start_loading(self):
+        print "Start loading..."
+
+    def run(self):
+        self.setup()
+        
+        self.monitor.start()
+        self.start_loading()
+        self.monitor.stop()
+
+        self.save_results()
+        self.cleanup()
 
 
-def change_rrd_dir(conf, rrd_dir):
-    section_rx = re.compile(r'<Plugin rrdtool>([^<]*\n)*</Plugin>')
-    section_mt = section_rx.search(conf)
-    if not section_mt:
-        raise Exception('There is no rrdtool section')
+class LocustBenchmarkRunner(BenchmarkRunner):
+    @staticmethod
+    def runbg(cmd, sockname="dtach"):
+        return sudo('dtach -n `mktemp -u /tmp/%s.XXXX` %s'  % (sockname, cmd))
 
-    section = section_mt.group()
+    def start_slave(self, slave_ip, slave_user, slave_password, host, master_ip, master_port):
+        with (settings(host_string=slave_ip, user=slave_user, password=slave_password)):
+            cmd = 'locust -f %s/scenario.py -H %s --no-web --slave --master-host=%s --master-port=%s'
+            slave_path = '/tmp/%s' % uuid.uuid4()
+            run('mkdir -p %s' % slave_path)
+            bm_path = self.benchmark.__path__[0]
+            put(bm_path, slave_path)
+            if slave_ip != master_ip:
+                put(self.benchmark.config.TABLE_LIST, self.benchmark.config.TABLE_LIST)
+            self.runbg(cmd % ('%s/%s' % (slave_path, self.benchmark.__name__.split('.')[-1]), host, master_ip, master_port))
 
-    dir_rx = re.compile(r'DataDir\s+"[^"]*"')
-    if dir_rx.search(section):
-        result = dir_rx.sub('DataDir "%s"' % rrd_dir, section)
-    else:
-        result = section.replace('</Plugin>',
-                                 '  DataDir "%s"\n</Plugin>' % rrd_dir)
+    def start_master(self, master_ip, master_port, master_user, master_password, host, requests_count):
+        with (settings(host_string=master_ip, user=master_user, password=master_password)):
+            cmd = 'locust -f %s/scenario.py -H %s --master -n %s --master-bind-host=%s --master-bind-port=%s'
+            sudo(cmd % (self.benchmark.__path__[0], host, requests_count, master_ip, master_port))
 
-    return section_rx.sub(result, conf)
+    def setup(self):
+        
+        host = self.cfg.get('locust', 'host_to_test')
+        self.benchmark.setup(host)
 
+        requests_count = self.cfg.get('locust', 'requests_count')
+        master_ip = self.cfg.get('locust', 'master_ip')
+        master_port = self.cfg.get('locust', 'master_port')
+        master_user = self.cfg.get('locust', 'master_user')
+        master_password = self.cfg.get('locust', 'master_password')
+        requests_count = self.cfg.get('locust', 'requests_count')
 
-def stop_monitoring():
-    cmd = ['service', 'collectd', 'stop']
-    subprocess.call(cmd, shell=False)
+        worker_ips = self.cfg.get('locust', 'worker_ips')
+        for slave_ip in worker_ips.split(','):
+            slave_user = self.cfg.get(slave_ip, 'user')
+            slave_password = self.cfg.get(slave_ip, 'password')
+            self.start_slave(slave_ip, slave_user, slave_password, host, master_ip, master_port)
 
+    def start_loading(self):
+        super(LocustBenchmarkRunner, self).start_loading()
+        host = self.cfg.get('locust', 'host_to_test')
+        requests_count = self.cfg.get('locust', 'requests_count')
+        master_ip = self.cfg.get('locust', 'master_ip')
+        master_port = self.cfg.get('locust', 'master_port')
+        master_user = self.cfg.get('locust', 'master_user')
+        master_password = self.cfg.get('locust', 'master_password')
+        requests_count = self.cfg.get('locust', 'requests_count')
+        self.start_master(master_ip, master_port, master_user, master_password, host, requests_count)
 
-def start_monitoring():
-    cmd = ['service', 'collectd', 'start']
-    subprocess.call(cmd, shell=False)
+    def cleanup(self):
+        host = self.cfg.get('locust', 'host_to_test')
+        self.benchmark.cleanup(host)
 
+class Monitor(object):
+    def start(self):
+        print "Start monitor"
 
-def get_collectd_conf(conf_path='/etc/collectd/collectd.conf'):
-    return open(conf_path, 'r').read()
-
-
-def set_collectd_conf(conf, conf_path='/etc/collectd/collectd.conf'):
-    open(conf_path, 'w').write(conf)
-
-
-def start_load(cfg, locust_file):
-    host = cfg.get('locust', 'host_to_test')
-    requests_count = cfg.get('locust', 'requests_count')
-    master_ip = cfg.get('locust', 'master_ip')
-    master_port = cfg.get('locust', 'master_port')
-    master_user = cfg.get('locust', 'master_user')
-    master_password = cfg.get('locust', 'master_password')
-
-    worker_ips = cfg.get('locust', 'worker_ips')
-    if not worker_ips:
-        # TODO: shutdown all
-        sys.exit(-1)
-
-    for slave_ip in worker_ips.split(','):
-        slave_user = cfg.get(slave_ip, 'user')
-        slave_password = cfg.get(slave_ip, 'password')
-        start_locust_slave(slave_ip, slave_user, slave_password, locust_file, host, master_ip, master_port)
-
-    start_locust_master(master_ip, master_port, master_user, master_password, locust_file, host, requests_count)
-
-
-def runbg(cmd, sockname="dtach"):
-    return run('dtach -n `mktemp -u /tmp/%s.XXXX` %s'  % (sockname, cmd))
-
-def start_locust_master(master_ip, master_port, master_user, master_password, locust_file, host, requests_count):
-    with (settings(host_string=master_ip, user=master_user, password=master_password)):
-#        with cd('locust'), prefix('source .venv/bin/activate'):
-#        with cd('locust'):
-        cmd = 'locust -f %s -H %s --master -n %s --master-bind-host=%s --master-bind-port=%s'
-        run(cmd % (locust_file, host, requests_count, master_ip, master_port))
+    def stop(self):
+        print "Stop monitor"
 
 
-def start_locust_slave(slave_ip, slave_user, slave_password, locust_file, host, master_ip, master_port):
-    with (settings(host_string=slave_ip, user=slave_user, password=slave_password)):
-#        with cd('locust'):
-         cmd = 'locust -f %s -H %s --no-web --slave --master-host=%s --master-port=%s'
-         slave_locust_file = '/tmp/%s.py' % uuid.uuid4()
-         put(locust_file, slave_locust_file)
-         runbg(cmd % (slave_locust_file, host, master_ip, master_port))
+class CollectD(Monitor):
+    def __init__(self, rrd_dir, orig_rrd_dir='/var/lib/collectd/rrd'):
+        self.rrd_dir = rrd_dir
+        self.orig_rrd_dir = orig_rrd_dir
+
+    def _set_rrd_dir(self, rrd_dir, conf_file='/etc/collectd/collectd.conf'):
+        with open(conf_file, 'r') as f:
+            conf = f.read()
+        section_rx = re.compile(r'<Plugin rrdtool>([^<]*\n)*</Plugin>')
+        section_mt = section_rx.search(conf)
+        if not section_mt:
+            raise Exception('There is no rrdtool section')
+        section = section_mt.group()
+        dir_rx = re.compile(r'DataDir\s+"[^"]*"')
+        if dir_rx.search(section):
+            result = dir_rx.sub('DataDir "%s"' % self.rrd_dir, section)
+        else:
+            result = section.replace('</Plugin>',
+                    '  DataDir "%s"\n</Plugin>' % self.rrd_dir)
+        with open(conf_file, 'w') as f:
+            f.write(section_rx.sub(result, conf))
+
+    def start(self):
+        super(CollectD, self).start()
+        cmd = ['service', 'collectd', 'stop']
+        subprocess.call(cmd, shell=False)
+
+        self._set_rrd_dir(self.rrd_dir)
+
+        cmd = ['service', 'collectd', 'start']
+        subprocess.call(cmd, shell=False)
+
+    def stop(self):
+        super(CollectD, self).stop()
+        cmd = ['service', 'collectd', 'stop']
+        subprocess.call(cmd, shell=False)
+
+        self._set_rrd_dir(self.orig_rrd_dir)
+
+        cmd = ['service', 'collectd', 'start']
+        subprocess.call(cmd, shell=False)
 
 
-def get_timestamp_str(timestamp=None):
-    if timestamp:
-        return timestamp.isoformat()
-    return datetime.datetime.now().isoformat().replace('-', '_').replace('.', '_').replace(':', '_')
-    
-
-def store_results(results_dir):
-    stop_monitoring()
-    os.rename(results_dir, '%s_%s' % (results_dir,
-        get_timestamp_str()))
+def get_timestamp():
+    return datetime.datetime.now().isoformat().replace('.', '_').replace(':', '_')
 
 
-def save_test_cfg(cfg_file, locust_file, results_dir):
-    dst = os.path.join(results_dir, os.path.basename(cfg_file))
-    cmd = ['cp', cfg_file, dst]
-    print ' '.join(cmd)
-    subprocess.call(cmd, shell=False)
+def main(config, benchmark):
 
-    dst = os.path.join(results_dir, os.path.basename(locust_file))
-    cmd = ['cp', locust_file, dst]
-    print ' '.join(cmd)
-    subprocess.call(cmd, shell=False)
-
-
-def main(cfg_file, locust_file):
-    cfg = ConfigParser.ConfigParser()
-    cfg.read(cfg_file)
-
-    base_dir =cfg.get('global', 'base_dir')
-    prefix = cfg.get('global', 'result_dir_prefix')
-    dir_name = '%s_%s' % (prefix, get_timestamp_str())
+    base_dir = config.get('global', 'base_dir')
+    prefix = config.get('global', 'result_dir_prefix')
+    dir_name = '%s_%s' % (prefix, get_timestamp())
     results_dir = os.path.join(base_dir, dir_name)
-
-    print ("Setup monitioring...")
-    stop_monitoring()
-
     rrd_dir = os.path.join(results_dir, 'rrd')
     os.makedirs(rrd_dir)
-    conf = get_collectd_conf()
-    set_collectd_conf(change_rrd_dir(conf, rrd_dir))
 
-    host = cfg.get('locust', 'host_to_test')
-    keystone_url = cfg.get('keystone', 'keystone_url')
-    user = cfg.get('keystone', 'user')
-    password = cfg.get('keystone', 'password')
-    domain_name = cfg.get('keystone', 'domain_name')
-    project_name = cfg.get('keystone', 'project_name')
-    project_id = cfg.get('keystone', 'project_id')
-    # subprocess.call(' '.join("python ", locust_file, "/setup.py", host, keystone_url, user, password, domain_name, project_name, project_id))
-
-    start_monitoring()
-
-    print ("Start loading...")
-    start_load(cfg, locust_file)
-
-    save_test_cfg(cfg_file, locust_file, results_dir)
-
-    print ("Saving results...")
-    store_results(results_dir)
-
-    # subprocess.call(' '.join("python ", locust_file, "/teardown.py", host))
-
-    print ("Done.")
-
+    if config.get('global', 'monitor') == 'collectd':
+        mon = CollectD(rrd_dir)
+    if config.get('global', 'loader') == 'locust':
+        runner = LocustBenchmarkRunner(results_dir, config, benchmark, mon)
+    runner.run()
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print "Usage: %s /path/to/config.cfg /path/to/locust_file.py" % sys.argv[0]
+        print "Usage: %s /path/to/config /path/to/benchmark" % sys.argv[0]
         sys.exit(-1)
-    main(sys.argv[1], sys.argv[2])
+    conf = ConfigParser.ConfigParser()
+    conf.read(sys.argv[1])
+    try:
+        benchmark = importlib.import_module(sys.argv[2])
+    except ImportError:
+        print "Benchmark is not a python package"
+        sys.exit(-1)
+    main(conf, benchmark)
